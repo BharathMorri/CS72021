aebfdabbed24ec9d527dc68067d2bd8bac0d7f5e
BharathMorri <127733657+BharathMorri@users.noreply.github.com>
2023-04-11 15:04:42 +0530

    Add files via upload

---
+++ code/assignS1.asm
@@ -0,0 +1,58 @@
+.include "/sdcard/Download/FWC/gate2/m328Pdef.inc"
+
+
+
+ldi r17, 0b11110011 ;identifying output pins 2,3
+out DDRD,r17 
+
+ldi r17,0b11111111  ;  activating pull ups
+out PORTD,r17
+
+ldi r16,0b00000001
+out DDRB,r16            ;declaring 8th pin as output
+
+start:                   ;loop for reading the input from pins 2,3 continously
+
+        in r17,PIND             ;reading the data from pins 2,3
+;Taking q
+        ldi r24,0b00000100
+        mov r18,r17
+        and r18,r24
+        ldi r25,0b00000010
+        loopq:  lsr r18
+        dec r25
+        brne    loopq
+        .DEF q = r18
+;Taking p
+        mov r19,r17
+        ldi r24,0b00001000
+        and r19,r24
+        ldi r25,0b00000011
+        loopp:  lsr r19
+        dec r25 
+        brne loopp
+
+        .DEF p = r19
+        
+        ldi r21,0x00
+        ldi r22,0x00
+        
+        .DEF T1 = r21
+        .DEF T2 = r22
+        
+
+        ;POS expression
+        ;F =(p'(p+q))'+q
+	mov T1,q
+	mov T2,p
+	mov R1,p
+	mov R2,q
+	com R1
+	or T1,T2
+	and T1,R1
+	com T1
+	or T1,R2
+        out PORTB,T1
+       
+
+        rjmp start
